e4f2bb856466d93c3332c59022e65cb7df14d214
Matias Viola <matiasvioladi@gmail.com>
2024-06-19 00:19:54 -0300

    feat: juego inicialmente listo, controladores,... en progreso

--- Juego_De_La_Vida.py
+++ Juego_De_La_Vida.py
@@ -31,6 +31,7 @@ Aquí están las reglas básicas del Juego de la Vida:
 
 '''
 TODO:
+    - [] ividir en carpetas por ejemplo los controladores, las vistas y los modelos
     - [] Como hacer para que el usuario cargue la configuración inicial?
 
 IDEAS: 
@@ -49,23 +50,107 @@ LIFE = (255, 255, 255)
 DEATH = (0, 0, 0)
 MATRIX_X = 8
 MATRIX_Y = 8
+SPEED = 60 / 60 # Velocidad de actualización en FPS
 
 # Valiables globales no modificables
-GEN_NUMBER = 0
+GENERATION_NUMBER = 0
 EVEN_GEN_MATRIX = np.zeros((MATRIX_Y, MATRIX_X))
 ODD_GEN_MATRIX = np.zeros((MATRIX_Y, MATRIX_X))
 CELL_SIZE = SCREEN_X // MATRIX_X
 
+# Inicializamos la matriz con valores aleatorios
+def initialize_matrices():
+    global EVEN_GEN_MATRIX, ODD_GEN_MATRIX
+    for row in range(MATRIX_Y):
+        for col in range(MATRIX_X):
+            EVEN_GEN_MATRIX[row][col] = np.random.randint(0, 2)
+            ODD_GEN_MATRIX[row][col] = 0
+
+initialize_matrices()
+
+def live_neighbors(matrix,x, y):
+    '''
+    Función que calcula la cantidad de vecinos vivos de una celda
+    '''
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+            if matrix[(y + i) % MATRIX_Y][(x + j) % MATRIX_X] == 1:
+                count += 1
+    return count
+
 # Funciones
+def put_cell(x, y):
+    '''
+    Sobrepoblación: Una célula viva muere si tiene más de tres vecinos vivos.
+    Estabilidad: Una célula viva con dos o tres vecinos vivos permanece viva en 
+    la siguiente generación.
+    Reproducción: Una célula muerta con exactamente tres vecinos vivos se 
+    convierte en una célula viva.
+    '''
+    if (GENERATION_NUMBER % 2) == 0:
+        oldMatrix = EVEN_GEN_MATRIX
+        newMatrix = ODD_GEN_MATRIX
+    else:
+        oldMatrix = ODD_GEN_MATRIX
+        newMatrix = EVEN_GEN_MATRIX
+    neighbours = live_neighbors(oldMatrix, x, y)
+    cell_is_alive = oldMatrix[y][x] == 1
+    if cell_is_alive:
+        if neighbours < 2 or neighbours > 3:
+            newMatrix[y][x] = 0
+        else:
+            newMatrix[y][x] = 1
+    else:
+        if neighbours == 3:
+            newMatrix[y][x] = 1
+        else:
+            newMatrix[y][x] = 0
+    return newMatrix[y][x]
+
+    
+
+    
 def draw_board():
     for row in range(MATRIX_Y):
         for col in range(MATRIX_X):
-            if (row + col) % 2 == 0:
+            if put_cell(row,col) == 1:
                 color = LIFE
             else:
                 color = DEATH
             pygame.draw.rect(screen, color, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
+# Control de simulación
+global userSpeed, running
+userSpeed = 0  # Inclemento en la velocidad de la simulación
+running = True # Control de la simulación
+
+def pause():
+    global userSpeed, running
+    print("Presiona Enter para continuar")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    return
+                if event.type == pygame.QUIT:
+                    running = False
+                    return
+
+def handle_event(event):
+    global userSpeed, running
+    if event.type == pygame.QUIT:
+        running = False
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE:
+            pause()
+        if event.key == pygame.K_UP:
+            userSpeed += 1
+        if event.key == pygame.K_DOWN:
+            if userSpeed > 0:
+                userSpeed -= 1
 
 # Inicalizamos pygame
 pygame.init()
@@ -78,21 +163,23 @@ pygame.display.set_caption('Juego de la Vida')
 clock = pygame.time.Clock()
 
 # Bucle principal del juego
-running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        handle_event(event)
     
-    # Dibujar el tablero
-    screen.fill(LIFE)  # Llenar la pantalla de blanco
-    draw_board()  # Dibujar el tablero
-    
-    # Actualizar la pantalla
-    pygame.display.flip()
-    
-    # Controlar la velocidad de actualización
-    clock.tick(60)  # 60 FPS
+    if running == True:
+        # Dibujar el tablero
+        screen.fill(LIFE)  # Llenar la pantalla de blanco
+        draw_board()  # Dibujar el tablero
+
+        # Actualizar el número de generación
+        GENERATION_NUMBER += 1
+        
+        # Actualizar la pantalla
+        pygame.display.flip()
+        
+        # Controlar la velocidad de actualización
+        clock.tick(SPEED+userSpeed) 
     
 # Salida de Pygame
 pygame.quit()
